88207ea066026c7fb2a32ea39f6ecaeee0937cf0
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-12 09:11:56 +1000

    Feat: Implement program general info function

---
+++ u1172227Q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+}
+
+void generalInfo() {
+    cout << "This program assigns seats for a comercial airplane";
+    cout << "The current seat assignments are as follows";
+
+
+}
