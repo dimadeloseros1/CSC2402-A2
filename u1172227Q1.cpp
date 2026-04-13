f3c1844ff7aa603989f3274361c1995bec2e2621
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-14 09:43:40 +1000

    Feat: Implement seat general info function

--- u1172227Q1.cpp
+++ u1172227Q1.cpp
@@ -3,28 +3,40 @@
 
 using namespace std;
 
-
+void generalInfo();
 void generalRowInfo();
+void passengerClassInfo();
 
 int main() {
+    generalInfo();
     generalRowInfo();
+    passengerClassInfo();
 }
 
 void generalInfo() {
-    cout << "This program assigns seats for a comercial airplane";
-    cout << "The current seat assignments are as follows";
+    cout << "This program assigns seats for a commercial airplane." << endl;
+    cout << "The current seat assignments are as follows." << endl << endl;
 }
 
 void generalRowInfo() {
-    cout << setw(19) <<  "A B C  D E F" << endl;
+    cout << setw(19) << "A B C  D E F" << endl;
     for (int i = 1; i <= 13; i++) {
         for (int j = 0; j < 6; j++) {
         }
         if (i <= 9) {
             cout << "Row  " << i << " * * *  * * *" << endl;
-        }
-        else {
+        } else {
             cout << "Row " << i << " * * *  * * *" << endl;
         }
     }
+    cout << endl;
+}
+
+void passengerClassInfo() {
+    cout << "* -- available seat" << endl;
+    cout << "X -- available seat" << endl << endl;
+
+    cout << "Row 1 and 2 are for first class passengers." << endl;
+    cout << "Row 3 and 7 are for business class passengers." << endl;
+    cout << "Row 8 and 13 are for economy class passengers." << endl;
 }
