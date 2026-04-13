ed9acd1a262ac6e2f46c9acb0c0e6bd5147c8760
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-14 09:33:30 +1000

    Feat: Implement general info grid function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
+#include <set>
+using namespace std;
 int main() {
-    
+    // set<string> names;
+    // names.insert("George");
+    // names.insert("Juan");
+    // names.insert("Ian");
+    //
+    // names.insert("Paul");
+    // names.insert("Billy");
+    // names.insert("Bob");
+    //
+    // set<string>::iterator pos;
+    // for (pos = names.begin(); pos != names.end(); pos++) {
+    //     cout << *pos << endl;
+    // }
+
+    int x = 10;
+    int *b = &x;
+
+    cout << *b;
 
 
     return 0;
--- u1172227Q1.cpp
+++ u1172227Q1.cpp
@@ -1,14 +1,30 @@
+#include <iomanip>
 #include <iostream>
+
 using namespace std;
 
 
-int main() {
+void generalRowInfo();
 
+int main() {
+    generalRowInfo();
 }
 
 void generalInfo() {
     cout << "This program assigns seats for a comercial airplane";
     cout << "The current seat assignments are as follows";
+}
 
-
+void generalRowInfo() {
+    cout << setw(19) <<  "A B C  D E F" << endl;
+    for (int i = 1; i <= 13; i++) {
+        for (int j = 0; j < 6; j++) {
+        }
+        if (i <= 9) {
+            cout << "Row  " << i << " * * *  * * *" << endl;
+        }
+        else {
+            cout << "Row " << i << " * * *  * * *" << endl;
+        }
+    }
 }
