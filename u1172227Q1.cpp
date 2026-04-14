ffd4e16f53663b656961ddee97a23de05480e3ff
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-14 19:46:37 +1000

    Feat: Refactor / Implement class functionality

--- u1172227Q1.cpp
+++ u1172227Q1.cpp
@@ -2,41 +2,43 @@
 #include <iostream>
 
 using namespace std;
+class FirstQuestion {
+public:
+    void generalInfo() {
+        cout << "This program assigns seats for a commercial airplane." << endl;
+        cout << "The current seat assignments are as follows." << endl << endl;
+    }
 
-void generalInfo();
-void generalRowInfo();
-void passengerClassInfo();
+    void generalRowInfo() {
+        cout << setw(19) << "A B C  D E F" << endl;
+        for (int i = 1; i <= 13; i++) {
+            for (int j = 0; j < 6; j++) {
+            }
+            // This line adjusts the positions of the stars on the grid
+            if (i <= 9) {
+                cout << "Row " << setw(2) << i << " * * *  * * *" << endl;
+            } else {
+                cout << "Row " << i << " * * *  * * *" << endl;
+            }
+        }
+        cout << endl;
+    }
+
+    void passengerClassInfo() {
+        cout << "* -- available seat" << endl;
+        cout << "X -- available seat" << endl << endl;
+
+        cout << "Row 1 and 2 are for first class passengers." << endl;
+        cout << "Row 3 and 7 are for business class passengers." << endl;
+        cout << "Row 8 and 13 are for economy class passengers." << endl;
+    }
+};
+
 
 int main() {
-    generalInfo();
-    generalRowInfo();
-    passengerClassInfo();
-}
-
-void generalInfo() {
-    cout << "This program assigns seats for a commercial airplane." << endl;
-    cout << "The current seat assignments are as follows." << endl << endl;
-}
-
-void generalRowInfo() {
-    cout << setw(19) << "A B C  D E F" << endl;
-    for (int i = 1; i <= 13; i++) {
-        for (int j = 0; j < 6; j++) {
-        }
-        if (i <= 9) {
-            cout << "Row " << setw(2) << i << " * * *  * * *" << endl;
-        } else {
-            cout << "Row " << i << " * * *  * * *" << endl;
-        }
-    }
-    cout << endl;
-}
-
-void passengerClassInfo() {
-    cout << "* -- available seat" << endl;
-    cout << "X -- available seat" << endl << endl;
-
-    cout << "Row 1 and 2 are for first class passengers." << endl;
-    cout << "Row 3 and 7 are for business class passengers." << endl;
-    cout << "Row 8 and 13 are for economy class passengers." << endl;
+    FirstQuestion question;
+    question.generalInfo();
+    question.generalRowInfo();
+    question.passengerClassInfo();
+    return 0;
 }
