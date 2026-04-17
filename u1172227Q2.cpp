ddf32e4b21b9be921d18bd30cb24279a5a79a811
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-17 23:49:46 +1000

    Feat: Implement Q2 skeleton

---
+++ u1172227Q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int inputArray(const string &prompt, const int alpha[20]) {
+    int num;
+
+    while (true) {
+        cout << prompt << endl;
+        cin >> num;
+
+    }
+}
+
+
+int main() {
+    int alpha[20];
+    int beta[20];
+    int matrix[10][4];
+
+    return 0;
+}
