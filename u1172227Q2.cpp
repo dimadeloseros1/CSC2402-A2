75838a711ace5436ba7ba11edc59c493556042f9
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-18 22:36:45 +1000

    Feat: Implement doubleArray function

--- u1172227Q2.cpp
+++ u1172227Q2.cpp
@@ -5,7 +5,8 @@ const int SIZE = 20;
 const int ROWS = 10;
 const int COLS = 4;
 
-int inputArray(int alpha[], int size);
+void inputArray(int alpha[], int size);
+void doubleArray(int beta[], int alpha[], int size);
 
 int main() {
     int alpha[SIZE];
@@ -13,20 +14,26 @@ int main() {
     int matrix[ROWS][COLS];
 
     inputArray(alpha, SIZE);
+    doubleArray(beta, alpha, SIZE);
 
     return 0;
 }
 
-class SecondQuestion {
-
-    void inputArray(int alpha[], int size) {
-
-        cout << "Enter " << size << " integers" << endl;
-        for (int i = 0; i < size; i++) {
-            cout << "Enter the value: ";
-            cin >> alpha[i];
-        }
+// Exercise "a"
+void inputArray(int alpha[], int size) {
+    cout << "Enter " << size << " integers" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "Enter the value: ";
+        cin >> alpha[i];
     }
-};
+}
 
+// Exercise "b"
+void doubleArray(int beta[], const int alpha[], int size) {
+    // int alphaCopy[] = ALP
 
+    for (int i = 0; i < size; i++) {
+        beta[i] = alpha[i] * 2;
+        cout << beta[i];
+    }
+}
