e81e9ae042812a25951537fa8f33324e61cc6027
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-20 01:07:10 +1000

    Feat: Implement default constructor with roman letter

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 //Main program
-  
+
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include "roman.h" 
+#include "roman.h"
 
-using namespace std; 
+using namespace std;
 
 //Top level functions prototype to be implemented
 romanType add(romanType, romanType);
@@ -28,26 +28,26 @@ int main()
     cout << endl;
     roman3.printRoman();
     cout << endl;
-    
+
     //Add the rest of the tests to get the outputs as in the example output
-    
+
     //Test the no parameter constructor
-        
+
     //Test change the romanType value and representation
-            
+
     //Check the string constructor for romanType
-                
+
     //Check the value constructor for romanType
-                    
+
     //Check the increment and decrement operations
-                        
+
     //use the top level add function to add two romanTypes
-                            
+
     //use the overloaded add methods using an integer and a romanType as param
-    
+
     //check the class method for the add function with romanType
-    
+
     //check add integer class method to a romanType
-        
+
     //check the times table using romanTypes
 }
--- romanimp.cpp
+++ romanimp.cpp
@@ -7,3 +7,6 @@
  
 using namespace std;
 
+romanType::romanType() {
+    romanNum = "I";
+}
