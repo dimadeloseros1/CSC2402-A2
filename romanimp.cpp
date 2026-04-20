66200154e4dd2fefde77e95b5da0018aa8c7fd44
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-20 22:17:18 +1000

    Feat: Implement func to handle subtractive roman pairs

--- romanimp.cpp
+++ romanimp.cpp
@@ -1,10 +1,9 @@
-
 //Roman Number Implementation file
 
-#include <iostream> 
+#include <iostream>
 #include <string>
 #include "roman.h"
- 
+
 using namespace std;
 
 romanType::romanType() {
@@ -14,11 +13,75 @@ romanType::romanType() {
 
 romanType::romanType(int nums) {
     nums = num;
+    romanNum = intToRoman(nums);
 }
 
 romanType::romanType(string str) {
     romanNum = str;
     romanToPositiveInteger();
+}
+
+/*
+ *  Next I will add the "setters"
+ *
+ */
+
+// This method accepts a numerical string and derives the integer value
+void romanType::setRoman(int number) {
+    num = number;
+    romanNum = intToRoman(number);
+}
+
+void romanType::setRoman(string str) {
+    romanNum = str;
+    romanToPositiveInteger();
+}
+
+// Conversion helper functions
+// This function is in charge of handling subtractive pairs of the roman numbers
+void romanType::romanToPositiveInteger() {
+    num = 0;
+
+    auto charArr = [](char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    };
+
+    for (int i = 0; i < romanNum.length(); i++) {
+        int curentVal = charArr(romanNum[i]);
+        int nextVal = 0;
+
+        if (i + 1 < romanNum.length()) {
+            nextVal = charArr(romanNum[i + 1]);
+        }
+        else {
+            nextVal = 0;
+        }
+
+
+        if (curentVal < nextVal) {
+            num -= curentVal;
+        }
+        else {
+            num += curentVal;
+        }
+    }
 }
 
 
